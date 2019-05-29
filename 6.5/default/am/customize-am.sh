5254f3644295c5202976528570f974db200adafd
Habeeb <habmuz@gmail.com>
2019-05-29 16:53:24 +0800

    adding custom path

---
+++ 6.5/default/am/customize-am.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# This is a placeholder that you can replace with your own script to copy in assets such as images
+# or a web.xml file.
+# Some Environment variables that are available:
+# CATALINA_HOME: The expanded openam war file is in $CATALINA_HOME/webapps/openam
+# OPENAM_HOME
+# DOMAIN: The cookie domain (includes leading .)
+# NAMESPACE: The Kubernetes namespace
+echo "AM customization script"
+
+CATALINA_HOME:$CATALINA_HOME/webapps/openam
+
+mkdir -p /var/tmp/patch && cp ./patch/idxAuthRequestNode-1.2.2.jar -d /var/tmp/patch
+cp -r /var/tmp/patch/idxAuthRequestNode-1.2.2.jar "$CATALINA_HOME"/webapps/openam/WEB-INF/lib
+
+echo "AM customization script end"
