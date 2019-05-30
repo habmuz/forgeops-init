db6239fb49234aff72cdbdb728c6f9d5eaa7598e
Habeeb <habmuz@gmail.com>
2019-05-30 23:42:35 +0800

    script issue

--- 6.5/default/am/customize-am.sh
+++ 6.5/default/am/customize-am.sh
@@ -8,9 +8,10 @@
 # NAMESPACE: The Kubernetes namespace
 echo "AM customization script"
 
-CATALINA_HOME:$CATALINA_HOME/webapps/openam
+#CATALINA_HOME:$CATALINA_HOME/webapps/openam
 
-mkdir -p /var/tmp/patch && cp ./patch/idxAuthRequestNode-1.3.0-SNAPSHOT.jar -d /var/tmp/patch
-cp -r /var/tmp/patch/idxAuthRequestNode-1.3.0-SNAPSHOT.jar "$CATALINA_HOME"/webapps/openam/WEB-INF/lib
+mkdir -p /var/tmp/patch 
+cp ./patch/idxAuthRequestNode-1.3.0-SNAPSHOT.jar /var/tmp/patch
+cp -r /var/tmp/patch/idxAuthRequestNode-1.3.0-SNAPSHOT.jar "$CATALINA_HOME"/webapps/ROOT/openam/WEB-INF/lib
 
 echo "AM customization script end"
