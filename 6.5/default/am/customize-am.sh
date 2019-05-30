e03c1a4cdc4eefde394d946146ec075ca9e4abf1
Habeeb <habmuz@gmail.com>
2019-05-30 23:32:23 +0800

    file name

--- 6.5/default/am/customize-am.sh
+++ 6.5/default/am/customize-am.sh
@@ -10,7 +10,7 @@ echo "AM customization script"
 
 CATALINA_HOME:$CATALINA_HOME/webapps/openam
 
-mkdir -p /var/tmp/patch && cp ./patch/idxAuthRequestNode-1.2.2.jar -d /var/tmp/patch
-cp -r /var/tmp/patch/idxAuthRequestNode-1.2.2.jar "$CATALINA_HOME"/webapps/openam/WEB-INF/lib
+mkdir -p /var/tmp/patch && cp ./patch/idxAuthRequestNode-1.3.0-SNAPSHOT.jar -d /var/tmp/patch
+cp -r /var/tmp/patch/idxAuthRequestNode-1.3.0-SNAPSHOT.jar "$CATALINA_HOME"/webapps/openam/WEB-INF/lib
 
 echo "AM customization script end"
